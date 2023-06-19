e28268feae293cafb4e576fe28c380f98052d16f
gjr <632149385@qq.com>
2023-06-19 13:44:11 +0800

    feat:转为vue项目

---
+++ README.md
@@ -0,0 +1,30 @@
+# pp
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 项目介绍
+```
+该项目目前第一个工具为注释加密工具，目标为两个方向，一个为增加更多的工具方便前端程序员，另一个为提高适用性，做到开箱即用，如做成vscode插件等。
+欢迎各位大佬前来指点
+```
---
+++ src/views/AboutView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="about">
+    <el-row>
+      <el-col :span="8">
+        <el-input v-model="inmessage" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="danger" @click="encrypt">加密</el-button>
+        <el-button type="success" @click="decrypt">解密</el-button>
+      </el-col>
+      <el-col :span="8">
+        <span>{{outmessage}}</span>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      inmessage:undefined,
+      outmessage:undefined
+    }
+  },
+  methods:{
+    encrypt(){
+      let result = this.inmessage.split('')
+      result.forEach((item,index,arr)=>{
+        arr[index]=item.charCodeAt()
+        arr[index] = arr[index] + 3
+        arr[index] = String.fromCharCode(arr[index].toString())
+      })
+      result=result.join('')
+      this.outmessage=result
+    },
+    decrypt(){
+       let result = this.inmessage.split('')
+        result.forEach((item, index, arr) => {
+            arr[index] = item.charCodeAt()
+            arr[index] = arr[index] - 3
+            arr[index] = String.fromCharCode(arr[index].toString())
+        });
+        result = result.join('')
+        this.outmessage = result
+    }
+  }
+}
+</script>
+<style scoped>
+.el-row{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
